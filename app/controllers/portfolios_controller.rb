94c22efadaa498cddf75fcc9be2117eb4badc721
austin bentley <bentleyaustin86@gmail.com>
2017-07-17 14:46:01 -0600

    Configured custom routes for pages

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,23 +2,27 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+
   def new
     @portfolio_item = Portfolio.new
   end
+
   def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
-        #format.json { render :show, status: :created, location: @blog }
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
       end
-   end
- end
+    end
+  end
+
  def edit
    @portfolio_item = Portfolio.find(params[:id])
  end
+
  def update
    @portfolio_item = Portfolio.find(params[:id])
    respond_to do |format|
@@ -33,5 +37,19 @@ class PortfoliosController < ApplicationController
  def show
    @portfolio_item = Portfolio.find(params[:id])
  end
+
+ def destroy
+  #perform the lookup
+  @portfolio_item = Portfolio.find(params[:id])
+
+  #destroy/delete the record
+  @portfolio_item.destroy
+
+  #redirect
+  respond_to do |format|
+    format.html { redirect_to portfolios_url, notice: 'Record was removed' }
+  end
+ end
+
 end
   
